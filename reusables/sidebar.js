d8ddca84885de3e71cbe86dc2bf8164be1d1b6f8
Sarah Evbouan <sarahevbouan@yahoo.com>
2025-04-10 17:16:32 +0100

    Admin Dashboard

--- reusables/sidebar.js
+++ reusables/sidebar.js
@@ -3,18 +3,18 @@ let sidebarGenerator = () => {
  sidebarContainer.innerHTML = `
         <div class="brand">
           <h4 class="brand-name">Unity<span>Coop</span></h4>
-          <img src="/images/logo.svg" alt="" class="brand-logo" />
+          <img src="images/logo.svg" alt="" class="brand-logo" />
         </div>
         <div class="condensed-nav-group">
           <div class="mobile-nav">
             <a href="#search-bar">
-                <img src="/images/search_icon.svg" class="search-icon" />
+                <img src="images/search_icon.svg" class="search-icon" />
             </a>
             <a href="/pages/notification.html">
-              <img src="/images/notifications.svg" class="notification-icon" />
+              <img src="images/notifications.svg" class="notification-icon" />
             </a>
           </div>
-          <img src="/images/menu_icon.svg" alt="" class="menu-hamburger" id="menu-hamburger" />
+          <img src="images/menu_icon.svg" alt="" class="menu-hamburger" id="menu-hamburger" />
         </div>
         <!-- Left sidebar navigation -->
         <nav class="sidebar-nav">
@@ -22,32 +22,32 @@ let sidebarGenerator = () => {
           <ul>
             <li class="navlink-group" id="overview">
               <a href="/index.html">
-                <img src="/images/overview_icon.svg" alt="" />Overview</a
+                <img src="images/overview_icon.svg" alt="" />Overview</a
               >
             </li>
             <li class="navlink-group" id="pending-requests">
               <a href="/pages/pending-request.html">
-                <img src="/images/pend_req_icon.svg" alt="" />Pending Requests</a
+                <img src="images/pend_req_icon.svg" alt="" />Pending Requests</a
               >
             </li>
             <li class="navlink-group" id="loans">
               <a href="/pages/loans.html">
-                <img src="/images/loan_icon.svg" alt="" />Loans</a
+                <img src="images/loan_icon.svg" alt="" />Loans</a
               >
             </li>
             <li class="navlink-group" id="transactions">
               <a href="/pages/transactions.html">
-                <img src="/images/transaction_icon.svg" alt="" />Transactions</a
+                <img src="images/transaction_icon.svg" alt="" />Transactions</a
               >
             </li>
             <li class="navlink-group" id="users">
               <a href="/pages/users.html">
-                <img src="/images/user_icon.svg" alt="" />Users</a
+                <img src="images/user_icon.svg" alt="" />Users</a
               >
             </li>
             <li class="navlink-group" id="settings">
               <a href="/pages/settings.html">
-                <img src="/images/settings_icon.svg" alt="" />Settings</a
+                <img src="images/settings_icon.svg" alt="" />Settings</a
               >
             </li>
           </ul>
